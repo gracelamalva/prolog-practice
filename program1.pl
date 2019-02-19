a0a2403567dede92c30d9db82c69225cbfe8d306
grace.lamalva <gracelamalva@gracelaalvasMBP.home>
2019-02-18 21:37:20 -0500

    update

--- program1.pl
+++ program1.pl
@@ -17,8 +17,8 @@ female(pat).
 male(jim).
 male(john).
 
-mother(X, Y) :- 
-//X is the mother of Y
+mother(X, Y) :- parent(X,Y) , female(X). 
+#X is the mother of Y
 offspring(Y, X) :- 
-// Y is an offspring of x
+#Y is an offspring of x
 
