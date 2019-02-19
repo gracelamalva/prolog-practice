2efc940ee21ce6d7978411ca91cc9c0f8537db92
grace.lamalva <gracelamalva@gracelamalvas-MacBook-Pro.local>
2019-02-19 10:34:56 -0500

    corrected

---
+++ compiletest
@@ -0,0 +1,8 @@
+parent(jill,john).
+parent(john, pam).
+parent(pam, bob).
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(bob, patt).
+parent(pat,jim).
--- program1.pl
+++ program1.pl
@@ -1,24 +1,28 @@
+% female definitions
 parent(jill,john).
-parent(john, pam).
-parent(pam, bob).
+parent(john,pam).
+parent(pam,bob).
 parent(tom,bob).
 parent(tom,liz).
 parent(bob,ann).
-parent(bob, patt).
+parent(bob,patt).
 parent(pat,jim).
 
+% female definitions
 female(jill).
 female(pam).
-male(tom).
-male(bob).
 female(liz).
 female(ann).
 female(pat).
+
+% male definitions
+male(tom).
+male(bob).
 male(jim).
 male(john).
 
-mother(X, Y) :- parent(X,Y) , female(X). 
-#X is the mother of Y
-offspring(Y, X) :- 
-#Y is an offspring of x
+%X is the mother of Y
+mother(X, Y) :- parent(X,Y) , female(X).
 
+%Y is an offspring of x
+offspring(Y, X) :-  parent(X,Y).
